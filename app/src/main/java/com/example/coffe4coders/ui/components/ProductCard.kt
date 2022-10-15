dce6723b24074f3cbd2a6c2004727bcde2276915
Alvaro Armijos <alvaro.armijos@zemoga.com>
2022-10-14 22:40:29 -0500

    Add parameters to ProductCard

--- app/src/main/java/com/example/coffe4coders/ui/components/ProductCard.kt
+++ app/src/main/java/com/example/coffe4coders/ui/components/ProductCard.kt
@@ -2,30 +2,103 @@ package com.example.coffe4coders.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.coffe4coders.R
-import org.w3c.dom.Text
+import com.example.coffe4coders.ui.theme.Coffe4CodersTheme
+import com.example.coffe4coders.ui.theme.PlatziBlue
+import com.example.coffe4coders.ui.theme.PlatziGreen
+
+enum class CountryISO(val iso: String){
+    COL("COL"),
+    BRA("BRA"),
+    CRI("CRI"),
+    NIC("NIC");
+
+    fun getBackgroundImage(): Int {
+        when(this){
+            COL -> return R.drawable.co
+            BRA -> return R.drawable.br
+            CRI -> return R.drawable.ri
+            NIC -> return R.drawable.ni
+
+        }
+    }
+
+    fun getCountryFlag(): Int {
+        when(this){
+            COL -> return R.drawable.flagco
+            BRA -> return R.drawable.flagbr
+            CRI -> return R.drawable.flagri
+            NIC -> return R.drawable.flagni
+
+        }
+    }
+
+    fun getSurfaceColor(): Color {
+        when(this){
+            COL, NIC -> return PlatziBlue
+            BRA, CRI -> return PlatziGreen
+        }
+    }
+}
 
 @Composable
-fun ProductCard() {
+fun ProductCard(name: String,
+                summary: String,
+                price: Double,
+                currency: String,
+                country: CountryISO
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
             .clickable { }
-            .size(480.dp)
+            .size(480.dp),
+        elevation = 10.dp,
+        shape = MaterialTheme.shapes.small
     ) {
-        Image(painter = painterResource(id = R.drawable.co),
+        Image(painter = painterResource(id = country.getBackgroundImage()),
             contentDescription = null)
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = country.getSurfaceColor().copy(alpha = 0.2f)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(name, style = MaterialTheme.typography.h4)
+                Text(summary, style = MaterialTheme.typography.body1)
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Row {
+                        Image(painter = painterResource(id = country.getCountryFlag()),
+                            contentDescription = null ,
+                            modifier = Modifier.size(32.dp, 32.dp)
+                        )
+                        Text("$ $price $currency",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.End,
+                            style = MaterialTheme.typography.h4)
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -34,5 +107,13 @@ fun ProductCard() {
 )
 @Composable
 fun ProductCardPreview(){
-    ProductCard()
+    Coffe4CodersTheme {
+        ProductCard(
+            name = "Café de Brasil",
+            summary = "Café de las montañas",
+            price = 35.0,
+            currency = "USD",
+            country = CountryISO.BRA
+        )
+    }
 }
