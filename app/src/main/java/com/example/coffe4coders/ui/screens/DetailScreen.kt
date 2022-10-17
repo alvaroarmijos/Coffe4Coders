1ddc1ecd094548c57598a4ef775500fd3b79a599
Alvaro Armijos <alvaro.armijos@zemoga.com>
2022-10-17 15:30:50 -0500

    Create DetailScreen

--- app/src/main/java/com/example/coffe4coders/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/coffe4coders/ui/screens/DetailScreen.kt
@@ -1,6 +1,9 @@
 package com.example.coffe4coders.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -8,11 +11,17 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.coffe4coders.ui.components.CountryISO
-import com.example.coffe4coders.ui.components.CustomAppBar
+import com.example.coffe4coders.ui.components.*
+import com.example.coffe4coders.ui.theme.Coffe4CodersTheme
 
 @Composable
 fun DetailScreen(
@@ -30,7 +39,49 @@ fun DetailScreen(
             }
         },
         content = {
-
+            Column(
+                modifier = Modifier.verticalScroll(
+                    rememberScrollState()
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                ){
+                    Image(
+                        painter = painterResource(id = countryISO.getBackgroundImage()),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    TitleText(title = "Café de Colombia")
+                    Text(text = "Lorem ipsum dolor sit amet",
+                        style = MaterialTheme.typography.caption)
+                    Spacer(modifier = Modifier.height(24.dp))
+                    BodyText(body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel ligula bibendum, euismod dolor sed, consequat ex. Ut ullamcorper, orci quis dictum cursus, neque leo lobortis est, a feugiat sem nisi et mauris. Nunc convallis eu tellus sit amet cursus.")
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = "$ 35 USD",
+                            style = MaterialTheme.typography.h5,
+                            textAlign = TextAlign.End
+                        )
+                        CustomButton(label = "Continuar") {
+                            navController.navigate("checkout"){
+                                launchSingleTop = true
+                            }
+                        }
+                        
+                    }
+                }
+            }
         }
     )
 
@@ -43,5 +94,7 @@ fun DetailScreen(
 fun DetailScreenView() {
     val navController = rememberNavController()
 
-    DetailScreen(navController, CountryISO.COL)
+    Coffe4CodersTheme {
+        DetailScreen(navController, CountryISO.COL)
+    }
 }
